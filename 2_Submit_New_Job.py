420b989b27848ba75d19e4a509f71b1eb1febe50
Greg Shoda <greg.shoda@gmail.com>
2025-02-11 09:26:19 -0500

    trying again

--- 2_Submit_New_Job.py
+++ 2_Submit_New_Job.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 
 st.header("Enter a Job Application you've Submitted to your Tracker:")
@@ -56,64 +55,3 @@ with st.form("Job Application"):
 
 
 
-# with cover_letter_placeholder:
-=======
-import streamlit as st
-
-st.header("Enter a Job Application you've Submitted to your Tracker:")
-
-pdf_option = st.radio("Input Method", ["Shortcut with PDF of application", "Submit Full Form"])
-
-if pdf_option == "Shortcut with PDF of application":
-    uploaded_application = st.file_uploader("Upload your Application PDF Here:")
-
-
-with st.form("Job Application"):
-    job_hyperlink = st.text_input("Link to Job Posting", "")
-    st.write()
-
-    job_title = st.text_input("Job Title", "")
-
-    job_location = st.text_input("Job Location", "")
-
-    job_company = st.text_input("Company Name", "")
-
-    resume_used = st.selectbox("Which Resume did you use for this job application", "Select a Resume")
-
-    uploaded_resume = st.file_uploader("Upload New Resume: ")
-
-    cover_letter_required = st.radio("Was a cover letter required on this application?",
-                                     ["Required", "Optional", "Not Available"],
-                                     captions = ["Company Required a Cover Letter in the application",
-                                                 "Cover Letter was optional",
-                                                 "There was no option to upload a cover letter with this application"]
-    )
-
-
-    if cover_letter_required in ["Required", "Optional"]:
-        cover_letter_used = st.selectbox("Which Cover Letter did you use for this job application?", ("Cover Letter 1", "Cover Letter 2"))
-
-        uploaded_cover_letter = st.file_uploader("Upload New Cover Letter: ")
-
-    additional_questions = st.radio("Did the Application have additional questions you had to answer (Non DEI Questions)?",
-                                    ["Yes", "No"])
-    
-    additional_question1 = st.text_input("What was the first additional question?")
-
-    additional_question1_response = st.text_input("What was your response to that question?")
-
-    job_sentiment_scale = st.slider("Job Feeling", min_value = 1, max_value = 10)
-
-    job_qualification_scale = st.slider("How Qualified did you feel for the Job?", min_value = 1, max_value = 10)
-
-    #This is the last section of the form with the submit button
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write(f"Application for {job_title} at {job_company} successfully submitted!")
-
-
-
-
-
-# with cover_letter_placeholder:
->>>>>>> ae0f55839786f5b8d5fb87086a9a0ff123e8e78f
--- connections_class.py
+++ connections_class.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 import psycopg2
 import pandas as pd
@@ -41,47 +40,3 @@ cursor = conn.cursor()
 cursor.execute("SELECT version();")
 print(cursor.fetchone())
 cursor.close()
-=======
-import requests
-import psycopg2
-import pandas as pd
-import numpy as np
-import os
-import datetime
-import streamlit as st
-from bs4 import BeautifulSoup
-
-class ConnectionsManager():
-    def __init__(self):
-        self.cnxn = None
-
-    #@st.cache_resource
-    def get_postgres(self):
-        if self.cnxn is None:
-            self.cnxn = psycopg2.connect(
-                host = "jobsearch-test.cz8su8kcabuv.us-east-2.rds.amazonaws.com",
-                database = "postgres",
-                user = "gdshoda",
-                password = "p)aK1g3yW]1(6UbIeOGQN2j[u*B]",
-                port = 5432
-            )
-        
-        return self.cnxn
-    
-
-
-    def fetch_data(self, query):
-        cursor = self.cnxn.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        cursor.close()
-        return result
-    
-instance = ConnectionsManager()
-conn = instance.get_postgres()
-
-cursor = conn.cursor()
-cursor.execute("SELECT version();")
-print(cursor.fetchone())
-cursor.close()
->>>>>>> ae0f55839786f5b8d5fb87086a9a0ff123e8e78f
