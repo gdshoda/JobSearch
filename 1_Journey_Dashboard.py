a836062b8136b818c5b2666875b1604b384c1949
Greg Shoda <greg.shoda@gmail.com>
2025-02-12 16:28:30 -0500

    Worked with first page to display some data from the database after calling the connections_class module

--- 1_Journey_Dashboard.py
+++ 1_Journey_Dashboard.py
@@ -2,15 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import psycopg2
+from connections_class import PostgresqlQueries
 
+#read database credentials from secrets.toml file
 
-
-
+postgres_creds = st.secrets["connections"]["postgresql"]
 
 
 st.markdown("# Job Search Journey")
 st.write("In this page I will list out all of the jobs I have applied for")
 
-conn = st.connection("postgresql", type = "sql")
-df = conn.query('SELECT * FROM job_details;')
+pq = PostgresqlQueries(postgres_creds)
+
+pq.connect()
+df = pq.get_job_details()
 df
+pq.disconnect()
